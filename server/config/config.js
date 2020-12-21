3dedc400e83b2f715dd7dd9c34343766fae9803f
cristian3457 <s16120279@alumnos.itsur.edu.mx>
2020-12-20 19:56:53 -0600

    Cadena de conexión actualizada

--- server/config/config.js
+++ server/config/config.js
@@ -30,6 +30,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    // urlDB = process.env.MONGO_URI;
+    urlDB = "mongodb://root:root@cluster0-shard-00-00.aila2.mongodb.net:27017,cluster0-shard-00-01.aila2.mongodb.net:27017,cluster0-shard-00-02.aila2.mongodb.net:27017/cafe?ssl=true&replicaSet=atlas-s6v91x-shard-0&authSource=admin&retryWrites=true&w=majority";
 }
 process.env.URLDB = urlDB;
